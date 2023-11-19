6552ef964e9f124af6ea1480bb01e37854ec9592
Debug Titan <127851912+debugtitan@users.noreply.github.com>
2023-11-19 21:04:05 +0100

    Delete backend/apps/utils/exceptions directory

--- backend/apps/utils/exceptions/facebook_error.py
+++
@@ -1,8 +0,0 @@
-
-class FacebookException(Exception):
-    class Errors:
-        invalid_redirect_uri = "invalid_redirect_uri"
-
-    def __init__(self, errors: list, message: str):
-        self.errors = errors
-        self.message = message
